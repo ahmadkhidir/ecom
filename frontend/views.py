df0d99720d15f431adc1f3d7d29f022fe1362268
Ahmad Khidir <khidirahmad05@gmail.com>
2022-01-31 08:47:16 +0100

    First commit

---
+++ frontend/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
+# Create your views here.
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, "home/index.html", context)
